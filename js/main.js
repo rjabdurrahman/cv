a00fb95f6e6fc52965137c860eaac47d48255923
rjabdurrahman <md.rahman0296@gmail.com>
2019-09-24 10:54:20 -0700

    Added: Database ref

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBpQOjE7MXFMK5nn8T6dvjeG9-8f8ymWW4",
+    authDomain: "chatapp-1f75d.firebaseapp.com",
+    databaseURL: "https://chatapp-1f75d.firebaseio.com",
+    projectId: "chatapp-1f75d",
+    storageBucket: "chatapp-1f75d.appspot.com",
+    messagingSenderId: "880223391613"
+};
+firebase.initializeApp(config);
+
+const db = firebase.database().ref('/cv/' + JSON.parse(localStorage.user).uid);
